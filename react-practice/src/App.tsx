a50608b716c15ade8f1b2e9a3c5b32d15528ae85
smeghead <smeghead7@gmail.com>
2021-10-20 22:38:31 +0900

    add auth.

--- react-practice/src/App.tsx
+++ react-practice/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
 
 import React, {useState, useEffect} from 'react'
-import {firestore } from './firebase/index'
+import { firestore } from './firebase/index'
 import { collection, query, getDocs } from "firebase/firestore";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
 //import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [myAccount, setMyAccount] = useState<firebase.User>();
 
   useEffect(() => {
     const searchUsers = async() => {
@@ -21,13 +24,39 @@ function App() {
     }
 
     searchUsers();
+
+
   }, []);
+
+  const googleLogin = () => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        setMyAccount(user);
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  }
+
   const userListElement = () => {
     if (users.length == 0) {
       return <p>LOADING...</p>
     } else {
-      return users.map((user, index) => {
-        return <p key={index}> name: {user.name}</p>
+      return users.map((user) => {
+        return <p key={user.name}> name: {user.name}</p>
       })
     }
   };
@@ -35,6 +64,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         { userListElement() }
+        { myAccount ? <p>loging in</p> : <button onClick={googleLogin}>Google login</button> }
       </header>
     </div>
   );
