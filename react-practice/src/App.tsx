bd697d9e0fcdb20a667ee20ae6965eaf796ffbce
smeghead <smeghead7@gmail.com>
2022-01-15 21:41:28 +0900

    use useReducer.

--- react-practice/src/App.tsx
+++ react-practice/src/App.tsx
@@ -1,62 +1,61 @@
 import './App.css';
 
-import React, {useState } from 'react'
+import React, {useReducer } from 'react'
 
+const stateDefault = {
+  stack: [],
+  buffer: '',
+  waitingNumberInput: true,
+};
+const update = (state, val: string) => {
+  console.log('==update==');
+  const stack = state.stack.slice(0);
+  switch (val) {
+    case '+':
+      if (stack.length === 0) {
+        stack.push(state.buffer);
+      }
+      stack.push(val);
+      console.log(JSON.stringify(stack));
+      return {stack: stack, buffer: state.buffer, waitingNumberInput: true};
+    case '=':
+      stack.push(state.buffer);
+      const answer = eval(stack.join(''));
+      return {stack: stack, buffer: answer, waitingNumberInput: true};
+    default:
+      const buffer = state.waitingNumberInput ? val : state.buffer + val;
+      return {...state, buffer: buffer, waitingNumberInput: false};
+  }
+};
 
 function App() {
-  const [display, setDisplay] = useState('');
-  const [buffer, setBuffer] = useState([]);
-  const [displayIsEmpty, setDisplayIsEmpty] = useState(true);
-
-  const update = (val: string) => {
-    const buf = buffer;
-    switch (val) {
-      case '+':
-        if (buf.length === 0) {
-          buf.push(display);
-        }
-        buf.push(val);
-        setBuffer(buf);
-        setDisplayIsEmpty(true);
-        break;
-      case '=':
-        buf.push(display);
-        setBuffer(buf);
-        const answer = eval(buffer.join(''));
-        setDisplay(answer);
-        setDisplayIsEmpty(true);
-        break;
-      default:
-        setDisplay(displayIsEmpty ? val : display + val);
-        setDisplayIsEmpty(false);
-        break;
-    }
-  };
+  const [calcState, dispatch] = useReducer(update, stateDefault);
 
   return (
     <div className="App">
       <div>電卓</div>
       <div>
+        stack:
+        <span id="stack">{calcState.stack.join(' ')}</span>
+      </div>
+      <div>
         buffer:
-        <span id="buffer">{buffer.join(' ')}</span>
+        <span id="buffer">{calcState.buffer}</span>
       </div>
       <div>
-        <input id="display" type="text" value={display} />
-      </div>
-      <div>
-        <input type="button" value="0" onClick={() => update('0')} />
-        <input type="button" value="1" onClick={() => update('1')} />
-        <input type="button" value="2" onClick={() => update('2')} />
-        <input type="button" value="3" onClick={() => update('3')} />
-        <input type="button" value="4" onClick={() => update('4')} />
-        <input type="button" value="5" onClick={() => update('5')} />
-        <input type="button" value="6" onClick={() => update('6')} />
-        <input type="button" value="7" onClick={() => update('7')} />
-        <input type="button" value="8" onClick={() => update('8')} />
-        <input type="button" value="9" onClick={() => update('9')} />
-        <input type="button" value="." onClick={() => update('.')} />
-        <input type="button" value="+" onClick={() => update('+')} />
-        <input type="button" value="=" onClick={() => update('=')} />
+        <input type="button" value="0" onClick={() => dispatch('0')} />
+        <input type="button" value="1" onClick={() => dispatch('1')} />
+        <input type="button" value="2" onClick={() => dispatch('2')} />
+        <input type="button" value="3" onClick={() => dispatch('3')} />
+        <input type="button" value="4" onClick={() => dispatch('4')} />
+        <input type="button" value="5" onClick={() => dispatch('5')} />
+        <input type="button" value="6" onClick={() => dispatch('6')} />
+        <input type="button" value="7" onClick={() => dispatch('7')} />
+        <input type="button" value="8" onClick={() => dispatch('8')} />
+        <input type="button" value="9" onClick={() => dispatch('9')} />
+        <input type="button" value="." onClick={() => dispatch('.')} />
+        <input type="button" value="+" onClick={() => dispatch('+')} />
+        <input type="button" value="=" onClick={() => dispatch('=')} />
 
       </div>
     </div>
