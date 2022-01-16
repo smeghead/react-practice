9f530b0e8db5a97d9f95c43e66aadbce6fe580e8
smeghead <smeghead7@gmail.com>
2022-01-16 16:45:23 +0900

    Addition of decimal points.

--- react-practice/src/digit/digit.tsx
+++ react-practice/src/digit/digit.tsx
@@ -1,17 +1,35 @@
 import React, {useState, useEffect } from 'react'
 
 const figures = [
-  [true, true, true, false, true, true, true],
-  [false, true, false, false, true, false, false],
-  [true, false, true, true, true, false, true],
-  [true, false, true, true, false, true, true],
-  [false, true, true, true, false, true, false],
-  [true, true, false, true, false, true, true],
-  [false, true, false, true, true, true, true],
-  [true, false, true, false, false, true, false],
-  [true, true, true, true, true, true, true],
-  [true, true, true, true, false, true, true],
+  [true, true, true, false, true, true, true, false],
+  [false, true, false, false, true, false, false, false],
+  [true, false, true, true, true, false, true, false],
+  [true, false, true, true, false, true, true, false],
+  [false, true, true, true, false, true, false, false],
+  [true, true, false, true, false, true, true, false],
+  [false, true, false, true, true, true, true, false],
+  [true, false, true, false, false, true, false, false],
+  [true, true, true, true, true, true, true, false],
+  [true, true, true, true, false, true, true, false],
+  [false, false, false, false, false, false, false, true],
+  [false, false, false, false, false, false, false, false],
 ];
+
+const digitStyle = {
+  float: 'left',
+  margin: 5,
+};
+const barContainarStyle = {
+  width: 40, 
+  height: 90, 
+  backgroundColor: '#333', 
+  position: 'relative',
+};
+const barStyle = {
+  position: 'absolute',
+  display: 'inline-block',
+};
+
 const Digit = props => {
   console.log('Digit', props);
   const [num, setNum] = useState(props.n)
@@ -22,15 +40,16 @@ const Digit = props => {
   }, [props]);
 
   return (
-    <div className="Digit" style={{float: 'left', margin: 5}}>
-      <div style={{width: 40, height: 90, backgroundColor: 'DimGray', position: 'relative'}}>
-        <span style={{position: 'absolute', display: 'inline-block', backgroundColor: figures[num][0] ? 'limegreen' : 'gray', width: 20, height: 10, top: 0, left: 10}}></span>
-        <span style={{position: 'absolute', display: 'inline-block', backgroundColor: figures[num][1] ? 'limegreen' : 'gray', width: 10, height: 30, top: 10, left: 0}}></span>
-        <span style={{position: 'absolute', display: 'inline-block', backgroundColor: figures[num][2] ? 'limegreen' : 'gray', width: 10, height: 30, top: 10, left: 30}}></span>
-        <span style={{position: 'absolute', display: 'inline-block', backgroundColor: figures[num][3] ? 'limegreen' : 'gray', width: 20, height: 10, top: 40, left: 10}}></span>
-        <span style={{position: 'absolute', display: 'inline-block', backgroundColor: figures[num][4] ? 'limegreen' : 'gray', width: 10, height: 30, top: 50, left: 0}}></span>
-        <span style={{position: 'absolute', display: 'inline-block', backgroundColor: figures[num][5] ? 'limegreen' : 'gray', width: 10, height: 30, top: 50, left: 30}}></span>
-        <span style={{position: 'absolute', display: 'inline-block', backgroundColor: figures[num][6] ? 'limegreen' : 'gray', width: 20, height: 10, top: 80, left: 10}}></span>
+    <div className="Digit" style={digitStyle}>
+      <div style={barContainarStyle}>
+        <span style={{...barStyle, backgroundColor: figures[num][0] ? 'limegreen' : 'DimGray', width: 20, height: 10, top: 0, left: 10}}></span>
+        <span style={{...barStyle, backgroundColor: figures[num][1] ? 'limegreen' : 'DimGray', width: 10, height: 30, top: 10, left: 0}}></span>
+        <span style={{...barStyle, backgroundColor: figures[num][2] ? 'limegreen' : 'DimGray', width: 10, height: 30, top: 10, left: 30}}></span>
+        <span style={{...barStyle, backgroundColor: figures[num][3] ? 'limegreen' : 'DimGray', width: 20, height: 10, top: 40, left: 10}}></span>
+        <span style={{...barStyle, backgroundColor: figures[num][4] ? 'limegreen' : 'DimGray', width: 10, height: 30, top: 50, left: 0}}></span>
+        <span style={{...barStyle, backgroundColor: figures[num][5] ? 'limegreen' : 'DimGray', width: 10, height: 30, top: 50, left: 30}}></span>
+        <span style={{...barStyle, backgroundColor: figures[num][6] ? 'limegreen' : 'DimGray', width: 20, height: 10, top: 80, left: 10}}></span>
+        <span style={{...barStyle, backgroundColor: figures[num][7] ? 'limegreen' : 'DimGray', width: 10, height: 10, top: 80, left: 0}}></span>
       </div>
     </div>
   );
--- react-practice/src/digit/display.tsx
+++ react-practice/src/digit/display.tsx
@@ -1,6 +1,24 @@
 import React, {useState, useEffect } from 'react'
 import Digit from './digit.tsx'
 
+const displayStyle = {
+  margin: '5px auto',
+  width: 300,
+  height: 100,
+  backgroundColor: '#333',
+};
+const parseIndex = (n) => {
+  if (n === ' ') {
+    return 11;
+  }
+  if (n === '.') {
+    return 10;
+  }
+  console.log('parseIndex', n);
+  console.log('parseIndex', parseInt(n, 10));
+  return parseInt(n, 10);
+}
+
 const Display = props => {
   const [num, setNum] = useState(props.n)
   
@@ -9,13 +27,13 @@ const Display = props => {
   }, [props]);
 
   return (
-    <div className="Display" style={{margin: '5px auto', width: 300, height: 100, backgroundColor: 'DimGray'}}>
-      <Digit n={num.toString().padStart(6, '0').substring(0, 1)} />
-      <Digit n={num.toString().padStart(6, '0').substring(1, 2)} />
-      <Digit n={num.toString().padStart(6, '0').substring(2, 3)} />
-      <Digit n={num.toString().padStart(6, '0').substring(3, 4)} />
-      <Digit n={num.toString().padStart(6, '0').substring(4, 5)} />
-      <Digit n={num.toString().padStart(6, '0').substring(5, 6)} />
+    <div className="Display" style={displayStyle}>
+      <Digit n={parseIndex(num.toString().padStart(6, ' ').substring(0, 1))} />
+      <Digit n={parseIndex(num.toString().padStart(6, ' ').substring(1, 2))} />
+      <Digit n={parseIndex(num.toString().padStart(6, ' ').substring(2, 3))} />
+      <Digit n={parseIndex(num.toString().padStart(6, ' ').substring(3, 4))} />
+      <Digit n={parseIndex(num.toString().padStart(6, ' ').substring(4, 5))} />
+      <Digit n={parseIndex(num.toString().padStart(6, ' ').substring(5, 6))} />
       <br style={{clear: 'both'}} />
     </div>
   );
