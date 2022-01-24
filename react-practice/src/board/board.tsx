596c1600655d460b47f80742f4832b33dd73d31e
smeghead <smeghead7@gmail.com>
2022-01-24 23:18:41 +0900

    repeat string flows.

--- react-practice/src/board/board.tsx
+++ react-practice/src/board/board.tsx
@@ -16,6 +16,7 @@ const generateBuffer = (str: string, font: {[name: string]: Letter}) => {
     if (!font) {
         return buffer;
     }
+    str += '    '; //文と文の繰り返し時の隙間を作る
     str.split('').forEach(s => {
       const charCode: string = s.charCodeAt(0).toString()
       if (!(font[charCode])) {
@@ -24,7 +25,7 @@ const generateBuffer = (str: string, font: {[name: string]: Letter}) => {
         return
       }
       const letter = font[charCode]
-      console.log(s, letter);
+      // console.log(s, letter);
 
       let i = 0
       for (; i < letter.bbx[3] + 2; i++) {
@@ -37,29 +38,40 @@ const generateBuffer = (str: string, font: {[name: string]: Letter}) => {
         buffer[i] += letter.dwidth[0] === 4 ? '0000' : '00000000';
       }
     })
-    console.log('buffer initialized', buffer);
+    // console.log('buffer initialized', buffer);
 
     return buffer;
 };
 
-const Display = (props: {str: string, font: any}) => {
+const Display = (props: {str: string, font: {[name: string]: Letter}}) => {
   const [buffer, setBuffer] = useState(generateBuffer(props.str, props.font))
+  const [offset, setOffset] = useState(50)
   useEffect(() => {
     setBuffer(generateBuffer(props.str, props.font))
   }, [props]);
 
+  useEffect(() => {
+    if (Object.keys(props.font).length === 0) {
+      return;
+    }
+    const timerId = setTimeout(() => {
+      setOffset(offset - 1)
+    }, 50)
+    return () => clearTimeout(timerId)
+  }, [offset, props.font])
+
   return (
     <div className="Board" style={boardStyle}>
-        <Line buffer={buffer[9]} />
-        <Line buffer={buffer[8]} />
-        <Line buffer={buffer[7]} />
-        <Line buffer={buffer[6]} />
-        <Line buffer={buffer[5]} />
-        <Line buffer={buffer[4]} />
-        <Line buffer={buffer[3]} />
-        <Line buffer={buffer[2]} />
-        <Line buffer={buffer[1]} />
-        <Line buffer={buffer[0]} />
+        <Line buffer={buffer[9]} offset={offset} />
+        <Line buffer={buffer[8]} offset={offset} />
+        <Line buffer={buffer[7]} offset={offset} />
+        <Line buffer={buffer[6]} offset={offset} />
+        <Line buffer={buffer[5]} offset={offset} />
+        <Line buffer={buffer[4]} offset={offset} />
+        <Line buffer={buffer[3]} offset={offset} />
+        <Line buffer={buffer[2]} offset={offset} />
+        <Line buffer={buffer[1]} offset={offset} />
+        <Line buffer={buffer[0]} offset={offset} />
     </div>
   );
 }
--- react-practice/src/board/line.tsx
+++ react-practice/src/board/line.tsx
@@ -9,77 +9,87 @@ const dotStyle = {
     background: '#333',
 }
 
-const Line = (props: {buffer: string}) => {
+const Line = (props: {buffer: string, offset: number}) => {
   const [buffer, setBuffer] = useState<string>('')
   useEffect(() => {
     setBuffer(props.buffer)
   }, [props]);
 
-  const lightning = (x: number) => {
+  const lightning = (x: number, offset: number) => {
     if (! buffer) {
         return '#333';
     }
-    if (buffer.length < x) {
-        return '#333';
+    // if (buffer.length < x) {
+    //     return '#333';
+    // }
+    let virtualScreenBuffer = ''
+    if (offset >= 0) {
+      virtualScreenBuffer = '0'.repeat(offset) + buffer
+    } else {
+      virtualScreenBuffer = buffer.substring((offset % buffer.length) * -1)
     }
-    // console.log(bit[x])
-    if (buffer.substring(x, x + 1) === '1') {
+    // 繰り返し
+    while (virtualScreenBuffer.length < 50) {
+      virtualScreenBuffer += buffer
+    }
+    // console.log(virtualScreenBuffer)
+    if (virtualScreenBuffer.substring(x, x + 1) === '1') {
         return 'radial-gradient(farthest-corner at 8px 8px, #ffffff 0, #fa8916 70%, #f87d02 100%)';
     }
     return '#333';
   };
   return (
     <div className="Line">
-        <div className="dot-off" style={{...dotStyle, background: lightning(0)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(1)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(2)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(3)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(4)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(5)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(6)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(7)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(8)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(9)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(10)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(11)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(12)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(13)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(14)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(15)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(16)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(17)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(18)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(19)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(20)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(21)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(22)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(23)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(24)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(25)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(26)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(27)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(28)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(29)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(30)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(31)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(32)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(33)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(34)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(35)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(36)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(37)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(38)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(39)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(40)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(41)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(42)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(43)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(44)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(45)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(46)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(47)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(48)}} />
-        <div className="dot-off" style={{...dotStyle, background: lightning(49)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(0, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(1, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(2, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(3, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(4, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(5, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(6, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(7, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(8, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(9, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(10, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(11, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(12, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(13, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(14, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(15, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(16, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(17, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(18, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(19, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(20, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(21, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(22, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(23, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(24, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(25, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(26, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(27, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(28, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(29, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(30, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(31, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(32, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(33, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(34, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(35, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(36, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(37, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(38, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(39, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(40, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(41, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(42, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(43, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(44, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(45, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(46, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(47, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(48, props.offset)}} />
+        <div className="dot-off" style={{...dotStyle, background: lightning(49, props.offset)}} />
 
     </div>
   );
